a0635102b5c2cded2efa217d3361d7760ce24531
KamilKrauze <72396466+KamilKrauze@users.noreply.github.com>
2025-02-08 09:34:19 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -27,7 +27,7 @@ project "VulkanRenderer"
     }
 
     libdirs {
-        "./thirdparty/include/glfw/src/Release",
+        "./thirdparty/include/glfw/build/src/Release",
         "./thirdparty/lib/"
     }
 
